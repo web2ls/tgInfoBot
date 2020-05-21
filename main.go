7867f6d73ac86bf4a52f88377b97f5f7e13b9656
web2ls <web2ls@yandex.ru>
2020-05-21 22:10:03 +0300

    added function for get actually currency exchange

--- main.go
+++ main.go
@@ -17,13 +17,29 @@ type exactTime struct {
 	Time int `json:"time"`
 }
 
+type currencyItem struct {
+	ID       string
+	NumCode  string
+	CharCode string
+	Nominal  int
+	Name     string
+	Value    float32
+	Previous float32
+}
+
+type exhanges struct {
+	Valute map[string]currencyItem
+}
+
 var buttonsMarkup = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
 		tgbotapi.NewKeyboardButton("Точное время (московское)"),
+		tgbotapi.NewKeyboardButton("Курсы валют"),
 	),
 )
 
-var timeURL = "https://yandex.com/time/sync.json?geo=213"
+const timeURL = "https://yandex.com/time/sync.json?geo=213"
+const currencyURL = "https://www.cbr-xml-daily.ru/daily_json.js"
 
 const msInSec = 1000
 
@@ -44,6 +60,21 @@ func getTimeAsString(timestamp int) string {
 	second := checkOnAdditionalZero(strconv.Itoa(fullDateByLocation.Second()))
 
 	return fmt.Sprintf("%s : %s : %s", string(hour), string(minute), string(second))
+}
+
+func getCurrencyValue(data exhanges, mainCurrency []string) string {
+	var usdValue float32
+	var eurValue float32
+	for _, value := range mainCurrency {
+		if value == "USD" {
+			usdValue = data.Valute[value].Value
+		} else if value == "EUR" {
+			eurValue = data.Valute[value].Value
+		}
+		fmt.Println(value)
+	}
+
+	return fmt.Sprintf("$%.2f ¢%.2f", usdValue, eurValue)
 }
 
 func main() {
@@ -78,6 +109,29 @@ func main() {
 			}
 		} else {
 			switch update.Message.Text {
+			case "Курсы валют":
+				mainCurrency := []string{"USD", "EUR"}
+				data := exhanges{}
+
+				res, err := http.Get(currencyURL)
+				if err != nil {
+					log.Println(err)
+				}
+
+				defer res.Body.Close()
+
+				body, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					log.Println(err)
+				}
+
+				err = json.Unmarshal(body, &data)
+				if err != nil {
+					log.Println(err)
+				}
+
+				msg.Text = getCurrencyValue(data, mainCurrency)
+
 			case "Точное время (московское)":
 				data := exactTime{}
 
