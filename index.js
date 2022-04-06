91450012801d8887dadacd31abe89670e0b349b6
Alexey Slobodyansky <web2ls@yandex.ru>
2022-04-06 23:51:20 +0300

    Bot is started

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => {
-  let message = `Lets drop my friend`
-  ctx.reply(message)
+  let message = `I can help you!`;
+  ctx.reply(message);
 });
+
+bot.command('time', ctx => {
+  ctx.reply('Moscow time is here');
+})
+
+bot.launch();
