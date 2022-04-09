4513c9f555d588d83232edf969a05921da1a61a8
Alexey Slobodyansky <web2ls@yandex.ru>
2022-04-09 23:56:22 +0300

    added simple bot control

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => {
-  let message = `I can help you!`;
-  ctx.reply(message);
+  ctx.reply('Select ', Markup
+    .keyboard(['Точное московское время'])
+    .oneTime()
+    .resize()
+  )
 });
 
 bot.command('time', ctx => {
   ctx.reply('Moscow time is here');
-})
+});
+
+bot.on('message', (ctx) => ctx.reply('message'));
 
 bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
