5b169d1f2d8d95583c21e6cc1379be6937daadc0
Alexey Slobodyansky <web2ls@yandex.ru>
2022-04-10 19:10:28 +0300

    added display currency rates list

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const axios = require('axios');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const TIME_URL = 'https://yandex.com/time/sync.json?geo=213';
+const CURRENCY_URL = 'https://currency-exchange.p.rapidapi.com/exchange';
 const COMMANDS = {
-  'Точное московское время': getTime
+  'Точное московское время': getTime,
+  'Курсы валют': getCurrencyRates,
 };
 
 function getTime() {
@@ -17,14 +19,43 @@ function getTime() {
 
       resolve(new Date(res.data.time).toLocaleTimeString());
     }).catch(err => {
-      console.log(err);
-      reject('We have an error in process');
+      reject(false);
     });
   })
 };
 
+function getCurrencyRates() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const USD = await getCurrency('USD');
+      const EUR = await getCurrency('EUR');
+      resolve(`USD: ${USD.toFixed(2)} , EUR: ${EUR.toFixed(2)}`);
+    } catch (err) {
+      reject(false);
+    }
+  })
+};
+
+function getCurrency(currency = 'USD') {
+  return new Promise((resolve, reject) => {
+    const headers = {
+      'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.X_RAPID_KEY,
+    };
+    const params = {
+      from: currency,
+      to: 'RUB'
+    };
+    axios.get(CURRENCY_URL, { params, headers }).then(res => {
+      resolve(res.data);
+    }).catch(err => {
+      reject(false);
+    })
+  })
+}
+
 function getMenu() {
-  return Markup.keyboard(['Точное московское время']);
+  return Markup.keyboard(['Точное московское время', 'Курсы валют']);
 };
 
 bot.start((ctx) => {
@@ -37,11 +68,15 @@ bot.command('time', ctx => {
 
 bot.on('message', async (ctx) => {
   if (!COMMANDS[ctx.message.text]) {
-    ctx.reply('404 Not Found');
+    ctx.reply('Sorry, but i dont know what you want...');
     return;
   };
 
-  ctx.reply(await COMMANDS[ctx.message.text]());
+  try {
+    ctx.reply(await COMMANDS[ctx.message.text]());
+  } catch (err) {
+    ctx.reply('We have an error in process');
+  }
 })
 
 bot.launch();
