b2fa0db52a7db07474f93425b835be4beed7a896
Alexey Slobodyansky <web2ls@yandex.ru>
2022-04-10 17:02:17 +0300

    added posibility to show precise time

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config();
+const axios = require('axios');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const TIME_URL = 'https://yandex.com/time/sync.json?geo=213';
+const COMMANDS = {
+  'Точное московское время': getTime
+};
+
+function getTime() {
+  return new Promise((resolve, reject) => {
+    axios.get(TIME_URL).then(res => {
+      const time = res.data && res.data.time || null;
+      if (!time) reject('We have an error in process');
+
+      resolve(new Date(res.data.time).toLocaleTimeString());
+    }).catch(err => {
+      console.log(err);
+      reject('We have an error in process');
+    });
+  })
+};
+
+function getMenu() {
+  return Markup.keyboard(['Точное московское время']);
+};
 
 bot.start((ctx) => {
-  ctx.reply('Select ', Markup
-    .keyboard(['Точное московское время'])
-    .oneTime()
-    .resize()
-  )
-});
+  ctx.reply('Select ', getMenu());
+})
 
 bot.command('time', ctx => {
   ctx.reply('Moscow time is here');
 });
 
-bot.on('message', (ctx) => ctx.reply('message'));
+bot.on('message', async (ctx) => {
+  if (!COMMANDS[ctx.message.text]) {
+    ctx.reply('404 Not Found');
+    return;
+  };
+
+  ctx.reply(await COMMANDS[ctx.message.text]());
+})
 
 bot.launch();
 
